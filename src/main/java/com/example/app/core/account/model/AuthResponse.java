09c4254bc6b1cd54b8f928ed74590f1debce6b88
David Attali <dattali93@gmail.com>
2024-10-10 10:21:43 -0400

    Auth response and request models

---
+++ src/main/java/com/example/app/core/account/model/AuthResponse.java
@@ -0,0 +1,17 @@
+package com.example.app.core.account.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String refreshToken;
+}
+
