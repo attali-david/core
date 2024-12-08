2db6f860a0bfc26038a31efc2555c65750fa7ac0
David Attali <dattali93@gmail.com>
2024-12-08 00:18:40 -0500

    Migrations

--- src/main/resources/db/migration/V1__user_table.sql
+++ src/main/resources/db/migration/V1__user_table.sql
@@ -3,7 +3,7 @@ CREATE TYPE privilege AS ENUM ('SYSTEM', 'ADMIN', 'USER', 'GUEST');
 CREATE TABLE app_user (
     id SERIAL PRIMARY KEY,
     created_at_local TIMESTAMP,
-    created_at_server TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_at_server TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     password TEXT,
     email TEXT UNIQUE NOT NULL,
     name TEXT,
--- src/main/resources/db/migration/V2__token_table.sql
+++ src/main/resources/db/migration/V2__token_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE refresh_token (
     id SERIAL PRIMARY KEY,
     token TEXT NOT NULL,
     user_fk INT REFERENCES app_user (id) ON DELETE CASCADE NOT NULL,
-    created_at_server TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at_server TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
